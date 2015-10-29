862e66b6f735f6536171f092ea626ca8f05fda22
Thuc Tran <thuctran289@gmail.com>
2015-10-29 16:24:59 -0400

    Push deliverables

--- HW/HW4/run_reg.sh
+++ HW/HW4/run_reg.sh
@@ -1,2 +1,2 @@
-iverilog -o regfile.t.vvp regfile.t.v regfile.v decoder1to32.v register32.v mux32to1by32.v register32zero.v
+iverilog -o regfile.t.vvp regfile.t.v regfile.v decoders.v register32.v mux32to1by32.v register32zero.v
 vvp regfile.t.vvp
